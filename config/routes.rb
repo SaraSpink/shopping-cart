28410201d2896692c1d346a77284823ab83a6031
Qudsia Ahmad and Sara Spink <qudsia@>
2017-12-04 12:43:07 -0800

    get an avatar working with paperclip and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  root 'landing_page#index'
   resources :accounts
   resources :order_items
   resources :orders
   resources :products
+  resources :landing_page, :only => :index
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
