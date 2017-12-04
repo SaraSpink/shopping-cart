fa56259c85bb070c5bf7b288a80cc92047f64baf
Qudsia Ahmad and Sara Spink <sara@>
2017-12-04 10:40:18 -0800

    use scaffold to set up order items and accounts and set the root route to products index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :accounts
+  resources :order_items
   resources :orders
   resources :products
   devise_for :users
---
+++ spec/factories/order_items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :order_item do
+    quantity 1
+    product_id 1
+    order_id 1
+  end
+end
