9bc4d7c49c20fb82fa2e9080fe77ce4bdc33f253
Qudsia Ahmad and Sara Spink <sara@>
2017-12-04 10:01:58 -0800

    add a scaffold for products

---
+++ spec/factories/products.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :product do
+    price ""
+    name "MyString"
+  end
+end
---
+++ spec/views/products/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "products/show", type: :view do
+  before(:each) do
+    @product = assign(:product, Product.create!(
+      :price => "",
+      :name => "Name"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(//)
+    expect(rendered).to match(/Name/)
+  end
+end
